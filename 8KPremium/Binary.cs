a40238d335f14e1c077bd7bc042e88e6a2748e16
rafal <rafal.kalata.itservice@gmail.com>
2023-12-01 23:33:52 +0100

    Add project files.

---
+++ 8KPremium/Binary.cs
@@ -0,0 +1,29 @@
+﻿namespace _8KPremium
+{
+    internal class Binary
+    {
+        public int BinaryGap(int number) 
+        {
+            string numberString = number.ToString();
+            int sumZero = 0;
+            int maxAmountZero = 0;
+
+            for (int i = 0; i< numberString.Length; i++) 
+            {
+                if (numberString[i] == '0')
+                {
+                    sumZero++;
+                    if (sumZero > maxAmountZero)
+                    {
+                        maxAmountZero = sumZero;
+                    }
+                }
+                else
+                {                    
+                    sumZero = 0;
+                }                    
+            }
+            return maxAmountZero;
+        }
+    }
+}
---
+++ 8KPremium/Program.cs
@@ -0,0 +1,17 @@
+﻿using _8KPremium;
+
+Binary binary = new();
+var rez = binary.BinaryGap(1000001111);
+var rez1 = binary.BinaryGap(1001011110);
+var rez2 = binary.BinaryGap(1011000001);
+var rez3 = binary.BinaryGap(1000001111);
+var rez4 = binary.BinaryGap(1001010000);
+var rez5 = binary.BinaryGap(1000010001);
+
+
+Console.WriteLine(rez);
+Console.WriteLine(rez1);
+Console.WriteLine(rez2);
+Console.WriteLine(rez3);
+Console.WriteLine(rez4);
+Console.WriteLine(rez5);
