e667bf67d60f2ba04ee53f1a4e507a0972e31540
rafal <rafal.kalata.itservice@gmail.com>
2023-12-05 00:21:05 +0100

    brace

---
+++ 8KPremium/Brace.cs
@@ -0,0 +1,37 @@
+﻿public class Brace
+{
+    public static bool validBraces(String braces)
+    {
+        Stack<char> stos = new();
+
+        foreach (char znak in braces)
+        {
+            if (znak == '(' || znak == '[' || znak == '{')
+            {
+                stos.Push(znak);
+            }
+            else if (znak == ')' || znak == ']' || znak == '}')
+            {
+                if (stos.Count == 0)
+                {
+                    return false;
+                }
+
+                char odpowiadajacyNawias = stos.Peek();
+
+                if (znak == ')' && odpowiadajacyNawias == '(' ||
+                    znak == ']' && odpowiadajacyNawias == '[' ||
+                    znak == '}' && odpowiadajacyNawias == '{')
+                {
+                    stos.Pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        return stos.Count == 0;
+    }
+}
